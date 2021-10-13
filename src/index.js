e7c82fc5d33f54a82536d1bb7ced124eb86fb02e
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-13 11:04:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const taskForm = document.getElementById('create-task-form')
+    const listBox = document.getElementById('list')
+
+    taskForm.addEventListener('submit', handleClick)
+    //listening for a submit event, when submit is clicked the handleClick function is called
+    function handleClick(e){
+
+        e.preventDefault()
+        //keeping submit button from doing its default action
+        const userInput = document.getElementById('new-task-description').value
+        // listBox.innerHTML += userInput
+        //too basic, its just adding it directly to the box on the same line
+        listBox.innerHTML += `<li class="tasks"> ${userInput} </li>`
+
+    }
+
+
+
+
 });
